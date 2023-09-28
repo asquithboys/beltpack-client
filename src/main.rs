b2fb29cd6e8124f24c9222e011dce887d69bcaa8
NinjaFB <oscar@oronberg.com>
2023-09-28 17:58:25 +1000

    Added testing function and power off animation
    Testing function is currently unimplemented

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use core::fmt;
 use std::{net::{IpAddr}, thread, time::Duration, process::Command};
 
-use embedded_graphics::{pixelcolor::BinaryColor, prelude::*, primitives::PrimitiveStyleBuilder};
+use embedded_graphics::{pixelcolor::BinaryColor, prelude::*, primitives::{PrimitiveStyleBuilder, Circle, PrimitiveStyle, Sector}};
 use embedded_graphics_core::primitives::Rectangle;
 use embedded_graphics_simulator::{BinaryColorTheme, SimulatorDisplay, Window, OutputSettingsBuilder};
 use u8g2_fonts::{fonts, FontRenderer, types::{HorizontalAlignment, VerticalPosition, FontColor}};
@@ -9,6 +9,24 @@ use u8g2_fonts::{fonts, FontRenderer, types::{HorizontalAlignment, VerticalPosit
 use local_ip_address::local_ip;
 
 use device_query::{DeviceQuery, DeviceState, Keycode, device_state};
+
+enum Button {
+    Power,
+    Ptt1,
+    Ptt2,
+    Unknown
+}
+
+impl fmt::Display for Button {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Button::Power => write!(f, "Power"),
+            Button::Ptt1 => write!(f, "PTT1"),
+            Button::Ptt2 => write!(f, "PTT2"),
+            Button::Unknown => write!(f, "No Signal")
+        }
+    }
+}
 
 #[derive(Debug)]
 struct User(String);
@@ -83,9 +101,9 @@ impl fmt::Display for Error {
             }
 
         }
-            let mut v: String = result.into_iter().rev().collect();
-            v.truncate(2);
-            return write!(f, "{}", v)
+        let mut v: String = result.into_iter().rev().collect();
+        v.truncate(2);
+        return write!(f, "{}", v)
     }
 }
 
@@ -94,25 +112,25 @@ fn handle_error<T,E>(result: Result<T, E>, display: &mut SimulatorDisplay<Binary
         Ok(_) => {
         }
         Err(err) => {
-    let fill = PrimitiveStyleBuilder::new()
-        .stroke_color(BinaryColor::On)
-        .fill_color(BinaryColor::On)
-        .build();
+            let fill = PrimitiveStyleBuilder::new()
+                .stroke_color(BinaryColor::On)
+                .fill_color(BinaryColor::On)
+                .build();
 
-    Rectangle::new(Point::new(0,50), Size::new(128,14))
-        .into_styled(fill)
-        .draw(&mut *display).unwrap();
+            Rectangle::new(Point::new(0,50), Size::new(128,14))
+                .into_styled(fill)
+                .draw(&mut *display).unwrap();
 
 
-    font2.render_aligned(
-        (String::from("ERROR: ") + err.to_string().as_str()).as_str(),
-        Point::new(display.bounding_box().center().x, 64),
-        VerticalPosition::Bottom,
-        HorizontalAlignment::Center,
-        FontColor::WithBackground{fg: BinaryColor::Off, bg: BinaryColor::On},
-        &mut *display,
-        )
-        .unwrap();
+            font2.render_aligned(
+                (String::from("ERROR: ") + err.to_string().as_str()).as_str(),
+                Point::new(display.bounding_box().center().x, 64),
+                VerticalPosition::Bottom,
+                HorizontalAlignment::Center,
+                FontColor::WithBackground{fg: BinaryColor::Off, bg: BinaryColor::On},
+                &mut *display,
+                )
+                .unwrap();
         }
     }
 }
@@ -139,6 +157,8 @@ fn main() -> Result<(), core::convert::Infallible> {
     //thread::sleep(Duration::from_secs(3));
     display.clear(BinaryColor::Off).unwrap();
 
+
+
     let text = "CAM 2";
     let target_user_1: User = User::new(text).unwrap(); 
 
@@ -162,14 +182,30 @@ fn main() -> Result<(), core::convert::Infallible> {
 
 
     let mut counter = 0;
-    let mut secs = 0;
+    let mut secs: u8 = 0;
 
     let target_user_2: User = User::new("STAGE").unwrap();
     let current_user: User = User::new("ROBSON").unwrap();
 
+
     loop {
         let keys: Vec<Keycode> = device_state.get_keys();
 
+        if keys.contains(&Keycode::Escape) {
+            power_display(&mut display, &secs.into(), 9f32); 
+            window.update(&mut display);
+            if secs >= 10 {
+                break
+            }
+            secs += 1;
+            thread::sleep(Duration::from_millis(100));
+            continue;
+
+        } else if secs != 0 {
+            secs = 0;
+            display.clear(BinaryColor::Off).unwrap();
+            counter = 10;
+        }
         if keys.contains(&Keycode::Left) {
             name_display(&mut display, &font1, &font2, &target_user_1, true);
             window.update(&mut display);
@@ -178,20 +214,11 @@ fn main() -> Result<(), core::convert::Infallible> {
             window.update(&mut display);
         } else {
             name_display(&mut display, &font1, &font2, &current_user, false);
-            handle_error(err(), &mut display, &font2);
             window.update(&mut display);
         }
 
-        if keys.contains(&Keycode::Escape) {
-            if secs >= 10 {
-                break
-            }
-            secs += 1;
-        } else {
-            secs = 0;
-        }
 
-        if counter == 10 {
+        if counter == 10 || secs != 0 {
             let p = Command::new("sh")
                 .arg("-c")
                 .arg("nmcli dev wifi list | awk '/\\*/{if (NR!=1) {print $8}}'")
@@ -284,7 +311,7 @@ fn signal_display(display: &mut SimulatorDisplay<BinaryColor>, font2: &FontRende
         .fill_color(BinaryColor::Off)
         .build();
 
-    Rectangle::new(Point::new(0,0), Size::new(26,14))
+    Rectangle::new(Point::new(0,0), Size::new(34,14))
         .into_styled(clear)
         .draw(&mut *display).unwrap();
 
@@ -319,4 +346,179 @@ fn ip_display(display: &mut SimulatorDisplay<BinaryColor>, font2: &FontRenderer,
         &mut *display,
         )
         .unwrap();
+}
+
+fn power_display(display: &mut SimulatorDisplay<BinaryColor>, currenta: &f32, maxa: f32) {
+    display.clear(BinaryColor::Off).unwrap();
+    let current = currenta + 1f32;
+    let max = maxa + 1f32;
+
+    let outline = Circle::with_center(display.bounding_box().center(), 48)
+        .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 2));
+    Sector::from_circle(outline.primitive, Angle::from_degrees(0.0), Angle::from_degrees(360.0f32 * (current / max)))
+        .into_styled(PrimitiveStyle::with_fill(BinaryColor::On))
+        .draw(&mut *display).unwrap();
+    outline.draw(&mut *display).unwrap();
+}
+
+
+fn test(window: &mut Window, display: &mut SimulatorDisplay<BinaryColor>, font1_small: &FontRenderer, font2: &FontRenderer) -> (Button, Button, Button) {
+    display.clear(BinaryColor::Off).unwrap();
+    let mut power = Button::Unknown;
+    let mut ptt1 = Button::Unknown;
+    let mut ptt2 = Button::Unknown;
+    let mut secs = 0;
+    font1_small.render_aligned(
+        "TEST",
+        display.bounding_box().center(),
+        VerticalPosition::Center,
+        HorizontalAlignment::Center,
+        FontColor::Transparent(BinaryColor::On),
+        &mut *display,
+        ).unwrap();
+    window.update(&mut *display);
+    thread::sleep(Duration::from_secs(1));
+    let device_state = DeviceState::new(); 
+    display.clear(BinaryColor::Off).unwrap();
+    font1_small.render_aligned(
+        "POWER",
+        display.bounding_box().center(),
+        VerticalPosition::Center,
+        HorizontalAlignment::Center,
+        FontColor::Transparent(BinaryColor::On),
+        &mut *display,
+        ).unwrap();
+    window.update(&mut *display);
+    loop {
+        let keys: Vec<Keycode> = device_state.get_keys();
+
+        if keys.contains(&Keycode::Escape) {
+            power = Button::Power;
+        } else if keys.contains(&Keycode::Left) {
+            power = Button::Ptt1;
+        } else if keys.contains(&Keycode::Right) {
+            power = Button::Ptt2;
+        }
+
+        if !matches!(power, Button::Unknown) && !keys.contains(&Keycode::Escape) && !keys.contains(&Keycode::Left) && !keys.contains(&Keycode::Right) {
+            break;
+
+        }
+
+        if secs >= 50 {
+            break;
+        }
+
+        thread::sleep(Duration::from_millis(100));
+        secs += 1;
+    }
+    thread::sleep(Duration::from_secs(1));
+    secs = 0;
+    display.clear(BinaryColor::Off).unwrap();
+    font1_small.render_aligned(
+        "PTT 1",
+        display.bounding_box().center(),
+        VerticalPosition::Center,
+        HorizontalAlignment::Center,
+        FontColor::Transparent(BinaryColor::On),
+        &mut *display,
+        ).unwrap();
+    window.update(&mut *display);
+    loop {
+        let keys: Vec<Keycode> = device_state.get_keys();
+
+        if keys.contains(&Keycode::Left) {
+            ptt1 = Button::Ptt1;
+        } else if keys.contains(&Keycode::Escape) {
+            ptt1 = Button::Power;
+        } else if keys.contains(&Keycode::Right) {
+            ptt1 = Button::Ptt2;
+        }
+
+        if !matches!(ptt1, Button::Unknown) && !keys.contains(&Keycode::Escape) && !keys.contains(&Keycode::Left) && !keys.contains(&Keycode::Right) {
+            break;
+
+        }
+        if secs >= 50 {
+            break;
+        }
+
+        thread::sleep(Duration::from_millis(100));
+        secs += 1;
+    }
+
+    secs = 0;
+    display.clear(BinaryColor::Off).unwrap();
+    font1_small.render_aligned(
+        "PTT 2",
+        display.bounding_box().center(),
+        VerticalPosition::Center,
+        HorizontalAlignment::Center,
+        FontColor::Transparent(BinaryColor::On),
+        &mut *display,
+        ).unwrap();
+    window.update(&mut *display);
+    loop {
+        let keys: Vec<Keycode> = device_state.get_keys();
+
+        if keys.contains(&Keycode::Right) {
+            ptt2 = Button::Ptt2;
+        } else if keys.contains(&Keycode::Escape) {
+            ptt2 = Button::Power;
+        } else if keys.contains(&Keycode::Left) {
+            ptt2 = Button::Ptt1;
+        }
+
+        if !matches!(ptt2, Button::Unknown) && !keys.contains(&Keycode::Escape) && !keys.contains(&Keycode::Left) && !keys.contains(&Keycode::Right) {
+            break;
+
+        }
+        if secs >= 50 {
+            break;
+        }
+
+        thread::sleep(Duration::from_millis(100));
+        secs += 1;
+    }
+
+    if  matches!(power, Button::Power) && matches!(ptt1, Button::Ptt1) && matches!(ptt2, Button::Ptt2) {
+        display.clear(BinaryColor::Off).unwrap();
+        font1_small.render_aligned(
+            "ALL GOOD",
+            display.bounding_box().center(),
+            VerticalPosition::Center,
+            HorizontalAlignment::Center,
+            FontColor::Transparent(BinaryColor::On),
+            &mut *display,
+            ).unwrap();
+        window.update(&mut *display);
+        thread::sleep(Duration::from_secs(1));
+    } else {
+        display.clear(BinaryColor::Off).unwrap();
+        font1_small.render_aligned(
+            "ERROR!",
+            display.bounding_box().center(),
+            VerticalPosition::Center,
+            HorizontalAlignment::Center,
+            FontColor::Transparent(BinaryColor::On),
+            &mut *display,
+            ).unwrap();
+        window.update(&mut *display);
+        thread::sleep(Duration::from_secs_f32(0.5));
+
+        display.clear(BinaryColor::Off).unwrap();
+        font2.render_aligned(
+            format!("Power: {}\nPTT1: {}\nPTT2: {}", power, ptt1, ptt2).as_str(),
+            display.bounding_box().center(),
+            VerticalPosition::Center,
+            HorizontalAlignment::Center,
+            FontColor::Transparent(BinaryColor::On),
+            &mut *display,
+            ).unwrap();
+        window.update(&mut *display);
+        thread::sleep(Duration::from_secs(2));
+        display.clear(BinaryColor::Off).unwrap();
+        window.update(&mut *display);
+    }
+    (power,ptt1,ptt2)
 }
